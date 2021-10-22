de75220ade97faf32ca13810e09c7f523e25c285
chynnalew <chynnalew@yahoo.com>
2021-10-22 09:17:40 -0700

    add fixed header, home index

--- PierresSweetAndSavory/Controllers/HomeController.cs
+++ PierresSweetAndSavory/Controllers/HomeController.cs
@@ -1,14 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PierresSweetAndSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PierresSweetAndSavory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresSweetAndSavoryContext _db;
+
+    public HomeController(PierresSweetAndSavoryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index() 
     { 
-      return View(); 
+      List<Treat> model = _db.Treats.ToList();
+      return View(model); 
     }
   }
 }
